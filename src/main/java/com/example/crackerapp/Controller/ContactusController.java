0eae15c3d4e383b3d6cd360e1284229000e43184
Jinthan10 <153614490+Jinthan10@users.noreply.github.com>
2026-02-11 18:13:32 +0530

    ContactusController.java

--- src/main/java/com/example/crackerapp/Controller/ContactusController.java
+++ src/main/java/com/example/crackerapp/Controller/ContactusController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "https://fronty.z29.web.core.windows.net")
-// @CrossOrigin
+@CrossOrigin
 
 public class ContactusController {
 
